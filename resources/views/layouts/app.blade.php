ada31e0bc8c9c281d8755107b3632652e67a0f20
MyUsername <romamargine@gmail.com>
2022-06-14 16:27:49 +0200

    Add initial layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        
+    </head>
+    <body>
+ 
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer>
+            <p>&copy; 2022</p>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="{{asset('js/app.js')}}"></script>
+
+    </body>
+</html>
